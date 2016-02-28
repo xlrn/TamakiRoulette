c928a47e6dfbf27b6f81319a3a672598c0ce5d8a
vincentw <vincentw@live.ca>
2016-02-28 01:05:21 -0800

    save choices popup

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,4 +1,25 @@
 var app = angular.module('app')
     .controller('HomeController', function($scope) {
+        $scope.saveChoices = function() {
+            $('#saveChoicesModal').modal('show');
+        };
 
+        $('#saveChoicesModal').on('hidden.bs.modal', function() {
+            $(this).find('form')[0].reset();
+        });
+
+        $scope.saveModalSubmit = function() {
+            var saveName = $('#saveChoicesModal').find('input#name').val().trim();
+            var choices = getChoices();
+        }
+
+        function getChoices() {
+            var inputs = $('#home').find('form#choices').find('input[type="text"]');
+            var choices = [];
+            for (var i = 0; i<inputs.length; i++) {
+                var value = $(inputs[i]).val().trim();
+                choices.push(value);
+            }
+            return choices;
+        }
     });
