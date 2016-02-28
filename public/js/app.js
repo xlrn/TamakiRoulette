1df1ff1f444780a5746ac648b4de063c0c8ebdc8
vincentw <vincentw@live.ca>
2016-02-27 17:55:18 -0800

    Base front-end

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeController'
+        })
+        .when('/signup', {
+            templateUrl: 'views/signup.html',
+            controller: 'SignupController'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}])
+.controller('App', function($scope) {
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
+});
---
+++ public/js/controllers/HomeController.js
@@ -0,0 +1,4 @@
+var app = angular.module('app')
+    .controller('HomeController', function($scope) {
+
+    });
