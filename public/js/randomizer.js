cd83227093542a61cf240bba5c2f461c30ceed2f
Graham <graham_lee23@msn.com>
2016-02-28 02:55:18 -0800

    Fixed bugs in random function

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,14 +1,30 @@
-function chooseOne() {
-  var One = Math.floor(Math.random() * 5) + 1;
-  return One;
+function chooseOne(numOfValues) {
+  //numOfValues = numOfValues - 1;
+  var One = Math.floor(Math.random() * numOfValues-1) + 1;
   console.log("random number is: "+ One);
+  return One;
+  //console.log("random number is: "+ One);
 }
 
 function setChoice() {
-  var One = chooseOne();
-  var Choice = document.getElementById("choice" + One).value;
-  var Change = document.getElementById("randomized").value;
-  Change = Choice;
+  var inputs=[document.getElementById("choice0").value,document.getElementById("choice1").value,document.getElementById("choice2").value,
+  document.getElementById("choice3").value,document.getElementById("choice4").value,document.getElementById("choice5").value];
+  var choices = [];
+  for(i = 0; i < inputs.length; i++){
+  if(inputs[i]){
+  	choices.push(inputs[i]); 
+  }
+  }
+   console.log("choices are : "+ choices +" and it's length is: "+choices.length);
+  var One = chooseOne(choices.length);
+  var Choice = choices[One];
+  //var Change = document.getElementById("randomized").value;
+  //Change = Choice;
   console.log("random value is: "+ Choice);
+  if(choices.length === 0){
+  	document.getElementById("randomized").value = "No inputs! Please make some choices!";
+  }
+  else{
   document.getElementById("randomized").value = Choice;
+  }
 }
