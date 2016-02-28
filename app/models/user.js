ee26c2d7c1757abb9dae786e8b85f2a970c146d1
vyangit <vyan.git@gmail.com>
2016-02-27 23:29:27 -0800

    Uncommented passport login

---
+++ app/models/user.js
@@ -0,0 +1,23 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+var User = new Schema({
+    username: String,
+    password: String,
+    roulettes: Array
+});
+
+// methods ======================
+// generating a hash
+User.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+User.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for account and expose it to our app
+module.exports = mongoose.model('User', User);
